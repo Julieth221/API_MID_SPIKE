191e95fdddda18481bd43e5a8985dbe65ffe2f34
Julieth221 <darlycuberos17@gmail.com>
2025-03-20 13:11:10 -0500

    Configuración para el controlador usuario

--- MID_SPIKE/routers/router.go
+++ MID_SPIKE/routers/router.go
@@ -9,19 +9,27 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/sena_2824182/API_MID_SPIKE/MID_SPIKE/controllers"
 )
 
 func init() {
-	ns := beego.NewNamespace("/v1") // beego.NSNamespace("/object",
-	// 	beego.NSInclude(
-	// 		&controllers.ObjectController{},
-	// 	),
-	// ),
-	// beego.NSNamespace("/user",
-	// 	beego.NSInclude(
-	// 		&controllers.UserController{},
-	// 	),
-	// ),
+	ns := beego.NewNamespace("/v1",
+
+		// Rutas para el controlador de usuarios
+		beego.NSNamespace("/usuarios",
+			beego.NSInclude(
+				&controllers.UsuarioController{},
+			),
+		),
+
+		// Rutas para el controlador de autenticación
+		beego.NSNamespace("/auth",
+			beego.NSInclude(
+				&controllers.Autenticacion_usuarioController{},
+			),
+		),
+	)
+	// beego.NSNamespace("/object",
 
 	beego.AddNamespace(ns)
 }
